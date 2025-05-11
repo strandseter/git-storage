9c6600b26baf31ea37ae55fe36fdb4224dc4acbf
strandseter <43789401+strandseter@users.noreply.github.com>
2025-05-11 21:58:16 +0200

    feat: export client and adapters

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { createClient } from './src/client';
+
+import { GithubAdapter } from './src/adapters/github';
+import { GitAdapter } from './src/adapters/git';
+
+export { createClient, GithubAdapter, GitAdapter };
