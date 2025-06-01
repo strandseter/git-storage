d8ff1c795082e216080e045eba60332674a502ed
strandseter <43789401+strandseter@users.noreply.github.com>
2025-06-01 23:18:35 +0200

    test: client update

--- test/integration/github/client.test.ts
+++ test/integration/github/client.test.ts
@@ -59,17 +59,16 @@ describe('create', () => {
   it('should create a new record', async () => {
     const client = createClient(GithubAdapter(BaseConfig));
 
-    const record = await client.create<Record>(
-      { filePath: remoteDataFilePaths.records, commitMessage: 'create: test' },
-      {
-        id: '3',
-        name: 'John Doe',
-        some_object: { some_field: 'some value' },
-        some_array: ['value1', 'value2', 'value3'],
-      },
-    );
+    const newRecord: Record = {
+      id: '3',
+      name: 'John Doe',
+      some_object: { some_field: 'some value' },
+      some_array: ['value1', 'value2', 'value3'],
+    };
 
-    const expectedRecords = [...(await readRecords()), record];
+    await client.create<Record>({ filePath: remoteDataFilePaths.records }, newRecord);
+
+    const expectedRecords = [...(await readRecords()), newRecord];
 
     const records = await client.getAll<Record>({ filePath: remoteDataFilePaths.records });
 
@@ -82,7 +81,7 @@ describe('create', () => {
     const existingId = '1';
 
     await expect(
-      client.create<Record>({ filePath: remoteDataFilePaths.records, commitMessage: 'create: test' }, {
+      client.create<Record>({ filePath: remoteDataFilePaths.records }, {
         id: existingId,
       } as Record),
     ).rejects.toThrow('Record already exists');
@@ -91,5 +90,32 @@ describe('create', () => {
     const expectedRecords = await readRecords();
 
     expect(records).toEqual(expectedRecords);
+  });
+});
+
+describe('update', () => {
+  it('should update a record', async () => {
+    const client = createClient(GithubAdapter(BaseConfig));
+
+    const updatedRecord: Record = {
+      id: '1',
+      name: 'John Doe',
+      some_object: { some_field: 'UPDATED' },
+      some_array: ['UPDATED'],
+    };
+
+    await client.update<Record>({ filePath: remoteDataFilePaths.records }, updatedRecord);
+
+    const [record] = await client.getAll<Record>({ filePath: remoteDataFilePaths.records });
+
+    expect(record).toEqual(updatedRecord);
+  });
+
+  it('should throw an error if the record does not exist', async () => {
+    const client = createClient(GithubAdapter(BaseConfig));
+
+    await expect(
+      client.update<Record>({ filePath: remoteDataFilePaths.records }, { id: '100' } as Record),
+    ).rejects.toThrow('Record not found');
   });
 });
